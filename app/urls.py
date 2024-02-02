9ef6890dc6325838aaeec4ceca62aa1ccd0d0e17
Samuel Sutch <sam@sutch.net>
2024-02-01 19:03:27 -0800

    fix url error

--- app/urls.py
+++ app/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path, include
+from django.urls import re_path, path, include
 
 from app import views
 
@@ -8,7 +8,7 @@ urlpatterns = [
     path('searches/new', views.new_search, name='search-new'),
     path('searches/<int:search_id>', views.edit_search, name='search-edit'),
     path('updates/feed', views.CustomFeed(), name='updates-feed'),
-    path('station/<slug:beacon_name>', views.station, name='station'),
+    re_path(r'station/(?P<beacon_name>[\w_-]+)', views.station, name='station'),
     path('updates_partial', views.updates_partial, name='updates_partial'),
     path('', include('plausible_proxy.urls')),
 ]
